1ff4c066a7406c26f24f20d130bd3b2acd674554
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-09-05 20:38:12 -0300

    add card in components

---
+++ src/components/card/Card1.jsx
@@ -0,0 +1,66 @@
+import { Box, Typography } from "@mui/material";
+
+import imgennashe from "../../assets/img.jpg";
+import { useState } from "react";
+
+export const Card1 = () => {
+  const [hovered, setHovered] = useState(false);
+
+  const card = {
+    width: 250,
+    height: 300,
+    overflow: "hidden",
+    border: "1px solid #fff",
+    position: "relative",
+    // zIndex: 1300, // Asegura que la tarjeta esté por encima del menú
+  };
+
+  const cardBody = {
+    width: "100%",
+    height: "100%",
+    color: "white",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  };
+
+  const cardImgText = {
+    position: "absolute",
+    transition: "2s",
+    backgroundColor: hovered ? "black" : "transparent",
+    right: hovered ? 0 : "-100%",
+    width: "100%",
+    height: "100%",
+    backgroundImage: `url(${imgennashe})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  };
+
+  return (
+    <Box
+      bgcolor={"#525050"}
+      sx={card}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <Box sx={cardImgText}>
+        <Typography gutterBottom variant="h4" color="white">
+          nashe
+        </Typography>
+      </Box>
+      <Box sx={cardBody}>
+        <Typography gutterBottom variant="h4" color="white">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="white">
+          Lizards are a widespread
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,13 +1,39 @@
-import { Box } from "@mui/material";
+import { Grid } from "@mui/material";
 import { Navbar } from "../components/menu/Navbar";
+import { Card1 } from "../components/card/Card1";
+import { CardHorizontal } from "../components/card/CardHorizontal";
 
 export const NewsPage = () => {
   return (
     <>
       <Navbar />
-      <Box bgcolor={"red"} pt={5}>
-        NewsPage
-      </Box>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        height={500}
+        flexDirection="row"
+      >
+        <Grid item container justifyContent="center" spacing={2} p={3}>
+          {/* Aquí estamos utilizando otro contenedor Grid para centrar y espaciar las tarjetas */}
+          <Grid item>
+            <Card1 />
+          </Grid>
+          <Grid item>
+            <Card1 />
+          </Grid>
+        </Grid>
+
+        <Grid item container justifyContent="center" spacing={2} p={3}>
+          {/* Aquí estamos utilizando otro contenedor Grid para centrar y espaciar las tarjetas */}
+          <Grid item>
+            <CardHorizontal />
+          </Grid>
+          <Grid item>
+            <CardHorizontal />
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 };
